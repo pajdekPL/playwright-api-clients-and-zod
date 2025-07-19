e7b8f6850df5096dc72215ebb778ff4c3e3682bf
PajdekPL <adam_pajda@outlook.com>
2025-07-19 12:17:11 +0200

    feat: update all packages

--- src/api/schemas/chirpy-error.schema.ts
+++ src/api/schemas/chirpy-error.schema.ts
@@ -2,7 +2,7 @@ import { z as schema } from "zod/v4";
 
 export const ChirpyErrorSchema = schema.object({
   error: schema.string(),
-  // someWronglyExpectedField: schema.string(), // easter egg to fix after executing tests ^^
+  someWronglyExpectedField: schema.string(), // easter egg to fix after executing tests ^^
 });
 
 export type ChirpyError = schema.infer<typeof ChirpyErrorSchema>;
